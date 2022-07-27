c25b0543cb2081ff06b7c3316cdeec3ac5c88cb9
Rajesh <rajeshceg3@gmail.com>
2022-07-27 08:56:39 +0000

    Initial project setup

---
+++ springBoot-security/src/main/java/com/security/Controller.java
@@ -0,0 +1,13 @@
+package com.security;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @RequestMapping("/")
+    String hello() {
+        return "Hello World!";
+    }
+}
