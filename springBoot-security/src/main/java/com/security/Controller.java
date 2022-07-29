4bb663490de906af1143fc21952b530344113696
Rajesh <rajeshceg3@gmail.com>
2022-07-29 06:57:05 +0000

    Gitignore updates

--- springBoot-security/src/main/java/com/security/Controller.java
+++ springBoot-security/src/main/java/com/security/Controller.java
@@ -1,13 +1,24 @@
 package com.security;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
 
-    @RequestMapping("/")
-    String hello() {
+    @GetMapping("/")
+    public String hello() {
         return "Hello World!";
     }
+
+    @GetMapping("/user")
+    public String user(){
+        return "Welcome to user page";
+    }
+
+    @GetMapping("/admin")
+    public String admin(){
+        return "Welcome to admin page";
+    }
+
 }
