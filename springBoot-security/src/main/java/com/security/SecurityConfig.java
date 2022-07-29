b111c61aae8138f12ed6ab165848d4de9f953da8
Rajesh <rajeshceg3@gmail.com>
2022-07-29 07:24:26 +0000

    Security Updates

--- springBoot-security/src/main/java/com/security/SecurityConfig.java
+++ springBoot-security/src/main/java/com/security/SecurityConfig.java
@@ -1,5 +1,29 @@
 package com.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("foo")
+                .password("bar")
+                .roles("USER")
+                .and()
+                .withUser("bar")
+                .password("foo")
+                .roles("ADMIN");
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+
     
 }
